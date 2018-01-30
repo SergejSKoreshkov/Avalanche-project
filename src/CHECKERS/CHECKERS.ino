2ddce5ffbdcc7fdf7bfd23f6ab644e44aa2090e6
SergejSKoreshkov <SergejSKoreshkov@gmail.com>
2018-01-30 13:00:32 +0200

    CHECKERS START

--- src/CHECKERS/CHECKERS.ino
+++ src/CHECKERS/CHECKERS.ino
@@ -1,9 +1,41 @@
-void setup() {
-  // put your setup code here, to run once:
 
+byte[][] field = new byte[8][8];
+
+void setup() 
+{
+  Serial.begin(9600);
+  CREATE_FIELD();                 //Генерируем клетчатое поле для игры
+  SHOW_FIELD();                   //Выводим поле в последовательный порт
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
-
+void loop() 
+{
+  
 }
+
+void CREATE_FIELD()
+{
+  for(int i = 0; i < 8; i++)
+  {
+    for(int j = 0; j < 8; j++)
+    {
+      if((j+i)%2 == 0)
+      {
+        field[i][j] = 1;
+      }
+    }
+  }
+}
+
+void SHOW_FIELD()
+{
+  for(int i = 0; i < 8; i++)
+  {
+    for(int j = 0; j < 8; j++)
+    {
+      Serial.print(String(field[i][j]) + "\t");
+    }
+    Serial.println();
+  }
+}
+
